ee0a364e71a9feb372ed6092da7318bb208646f3
praise-laurine <praiselaurine10@gmail.com>
2020-11-07 11:27:01 +0300

    added descroption to the goal array

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,5 @@
 export class Quote {
+    id:number;
+    quoteName:string;
+    description:string;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote }from  '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    {id:1,quoteName:'Push yourself beacause no one is going to fo it for you'},
-    {id:2,quoteName:'The harder you work on something, the greater you will feel when you achive it'},
-    {id:3,quoteName:'The way to get started is to quit talking and begin doing'},
-    {id:4,quoteName:'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You'},
-    {id:5,quoteName:'Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough'}
-    
+    {id:1,quoteName:'Push yourself beacause no one is going to fo it for you',description:'By Laurine Praise'},
+    {id:2,quoteName:'The harder you work on something, the greater you will feel when you achive it',description:'By John Campbell'},
+    {id:3,quoteName:'The way to get started is to quit talking and begin doing',description:'By Walt Disney'},
+    {id:4,quoteName:'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You',description:'By Steve Jobs'},
+    {id:5,quoteName:'Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough',description:'By Og Mandino'}
+
   ]
 
   constructor() { }
