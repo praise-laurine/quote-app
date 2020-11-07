552d26a4093f661e74fbeb17b7009d13606b9e61
praise-laurine <praiselaurine10@gmail.com>
2020-11-07 13:34:29 +0300

    added toggle details button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,} from '@angular/core';
 import { Quote }from  '../quote'; 
 @Component({
   selector: 'app-quote',
@@ -8,13 +8,19 @@ import { Quote }from  '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    {id:1,quoteName:'Push yourself beacause no one is going to fo it for you',description:'By Laurine Praise'},
-    {id:2,quoteName:'The harder you work on something, the greater you will feel when you achive it',description:'By John Campbell'},
-    {id:3,quoteName:'The way to get started is to quit talking and begin doing',description:'By Walt Disney'},
-    {id:4,quoteName:'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You',description:'By Steve Jobs'},
-    {id:5,quoteName:'Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough',description:'By Og Mandino'}
+    new Quote(1, 'Push yourself beacause no one is going to fo it for you', 'By Laurine Praise'),
+    new Quote(2, 'The harder you work on something, the greater you will feel when you achive it', 'By John Campbell'),
+    new Quote(3, 'The way to get started is to quit talking and begin doing', 'By Walt Disney'),
+    new Quote(4, 'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You', 'By Steve Jobs'),
+    new Quote(5, 'Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough', 'By Og Mandino')
 
-  ]
+  ];
+
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  
+  
 
   constructor() { }
 
