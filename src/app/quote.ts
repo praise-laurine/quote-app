b32a3cb573ceac1ed06f323606e110c45345ea01
praise-laurine <praiselaurine10@gmail.com>
2020-11-07 12:52:45 +0300

    added output property binding to quotes.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,10 @@
 export class Quote {
-    id:number;
-    quoteName:string;
-    description:string;
+    // id:number;
+    // quoteName:string;
+    // description:string;
 
-    // showDescription: boolean;
-    // constructor(public id: number,public quoteName: string,public description: string){
-    //     this.showDescription=false;
-    // }
+    showDescription: boolean;
+    constructor(public id: number,public quoteName: string,public description: string){
+        this.showDescription=false;
+    }
 }
