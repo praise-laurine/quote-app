038ee8370ed58c2d8e78390a7fe0c2ef24675008
praise-laurine <praiselaurine10@gmail.com>
2020-11-07 12:51:25 +0300

    added description below the quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,4 +2,9 @@ export class Quote {
     id:number;
     quoteName:string;
     description:string;
+
+    // showDescription: boolean;
+    // constructor(public id: number,public quoteName: string,public description: string){
+    //     this.showDescription=false;
+    // }
 }
