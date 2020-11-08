1881c3a724abee5beaa9b4c338bd9f457b2b913e
praise-laurine <praiselaurine10@gmail.com>
2020-11-09 01:22:22 +0300

    added backgroung image

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,9 +2,12 @@ export class Quote {
     // id:number;
     // quoteName:string;
     // description:string;
-
+    upvote: number;
+  downvote: number;
     public showDescription: boolean;
     constructor(public id: number,public quoteName: string,public description: string,public completeDate: Date){
         this.showDescription=false;
+        this.upvote = 0;
+        this.downvote = 0;
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -16,6 +16,15 @@ export class QuoteComponent implements OnInit {
 
   ];
 
+  upVote(index) {
+
+    
+    this.quotes[index].upvote++;
+  }
+  downVote(index) {
+    this.quotes[index].downvote++;
+  }  
+
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
@@ -28,6 +37,20 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     
+    }
+  }
+
+  highestUpVote() {
+    let highestUpVoteQuote = new Quote(0, '', '', new Date());
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i].upvote > highestUpVoteQuote.upvote) {
+        highestUpVoteQuote = this.quotes[i];
+      }
+    }
+    if (highestUpVoteQuote.upvote > 0) {
+      return highestUpVoteQuote;
+    } else {
+      return;
     }
   }
 
